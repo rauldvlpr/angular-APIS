904b6ebf3c53c3f453545ad754f20810289f1c00
Raul Castillo <rauln85@gmail.com>
2022-02-03 00:08:06 -0600

    Clase 2. Cambio de url de consumo y atributos

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
-
   @Input() product: Product = {
     id: '',
     price: 0,
-    image: '',
+    images: [],
     title: '',
-    category: '',
-    description: ''
+    category: {
+      id: 0,
+      name: '',
+      typeImg: '',
+    },
+    description: '',
   };
   @Output() addedProduct = new EventEmitter<Product>();
 
-  constructor() { }
+  constructor() {}
 
   onAddToCart() {
     this.addedProduct.emit(this.product);
   }
-
 }
